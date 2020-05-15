81832bec8b1a6a84af0c95107cfbfabc24104089
keshav12-36 <57794378+keshav12-36@users.noreply.github.com>
2020-05-15 22:51:49 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,13 +7,13 @@ class Config:
     API_HASH = os.environ.get('725797d62aca2512897545f93ce91f00')
     BOT_TOKEN = os.environ.get('826251513:AAFjJvHqVMYYOU6DAs1FoR36Nw1TVtL7udc')
     SESSION_NAME = os.environ.get('@rokulu_bot')
-    LOG_CHANNEL = int(os.environ.get('-34643225798'))
+    LOG_CHANNEL = os.environ.get('-34643225798')
     DATABASE_URL = os.environ.get('DATABASE_URL')
     AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
-    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
-    SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 15))
+    TRACK_CHANNEL = os.environ.get('TRACK_CHANNEL', False)
+    SLOW_SPEED_DELAY = os.environ.get('SLOW_SPEED_DELAY', 15)
     HOST = os.environ.get('HOST', '')
     
     SCRST_OP_FLDR = Path('screenshots/')
